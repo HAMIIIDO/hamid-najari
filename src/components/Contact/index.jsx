031ac9e2c0aa5ff8dc42883c5f9002ad6d175979
Hamido <hamidenajari9@gmail.com>
2023-11-02 17:17:59 +0100

    adding the map

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,16 +1,30 @@
 import Loader from 'react-loaders'
 import './index.scss'
+import emailjs from '@emailjs/browser'
 import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 const Contact = () => {
     
     const [letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef()
     
     useEffect(()=>{
         setTimeout(()=>{
           setLetterClass('text-animate-hover')
         }, 3000)
       })
+     const sendEmail =(e)=>{
+        e.preventDefault()
+        emailjs.sendForm('service_hbqpshh', 'contact_form_id', refForm.current, 'AV1CTKERo49GpX2cI')
+      .then(() => {
+          alert('Message successfully sent!');
+          window.location.reload(false)
+      }, (err) => {
+        console.log(err)
+          alert('Somthing went wrong, please try again!');
+      });
+     }
   return (
     <>
     <div className="container contact-page">
@@ -22,7 +36,7 @@ const Contact = () => {
                 I am intersted in freelance opportunities - especially ambitious or large projects. However, if you have other request or question, don't hesitate to contact me using bellow form either.
             </p>
             <div className="contact-form">
-                <form>
+                <form ref={refForm} onSubmit={sendEmail} >
                     <ul>
                         <li className='half'>
                         <input type="text" name='name' placeholder='Name' required /></li>
@@ -40,6 +54,24 @@ const Contact = () => {
                         </ul>
                 </form>
             </div>
+        </div>
+        <div className="info-map">
+
+            Hamid Najari,
+            <br />
+            Morocco,
+            <br />
+            Agadir 80000
+            <br />
+            <span>hamidenajari9@gmil.com</span>
+        </div>
+        <div className="map-wrap">
+            <MapContainer center={[30.378041, -9.479118]} zoom={13} >
+                <TileLayer url="https//{s}.tile.opentreetmap.org/{z}/{x}/{y}.png" />
+                <Marker position={[30.378041, -9.479118]}>
+                    <Popup>Hamido lives here, come over for a cup of coffee :)</Popup>
+                </Marker>
+            </MapContainer>
         </div>
     </div>
 
