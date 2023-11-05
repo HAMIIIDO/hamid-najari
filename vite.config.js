dc4225bcd134b0162015fc2a78b9d910b53a4e08
Hamido <hamidenajari9@gmail.com>
2023-11-05 18:20:12 +0100

    ff

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hamid-najari/",
-  server: {
-    port: 3000,
-  },
+  base: '/hamid-najari/',
 })
